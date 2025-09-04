de85096a16a419ed0073855e0e1b01482d19285b
Hyungwook Yu <105044562+hyungwook0221@users.noreply.github.com>
2025-09-04 16:20:22 +0900

    LGTM

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,7 +13,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Hello CPLATFORM
+  Welcome to ${PREFIX}'s app. Hello IBM BP
   <!-- END -->
 
   </div>
